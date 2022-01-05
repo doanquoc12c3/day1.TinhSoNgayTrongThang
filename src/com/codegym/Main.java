1b1484996129ba2f1ca6c575bef57e73bc486a5b
Jack King <ledoanquoc12c3@gmail.com>
2022-01-05 07:03:26 +0700

    new

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,55 @@
+package com.codegym;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap vao thang ban muon tim: ");
+        int thang = scanner.nextInt();
+        switch (thang){
+            case 1:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 2:
+                System.out.println("so ngay trong thang la: 28");
+                break;
+            case 3:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 4:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 5:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 6:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 7:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 8:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 9:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 10:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 11:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            case 12:
+                System.out.println("so ngay trong thang la: 30");
+                break;
+            default:
+                System.out.println("invalid input");
+                break;
+
+        }
+
+    }
+}
